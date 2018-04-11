29f9c9ad569c40ac9c82f645253ceade3bd78244
Yasemin Varlik <varly065@newschool.edu>
2018-04-10 20:13:39 -0400

    article page

--- publication/assets/js/app.js
+++ publication/assets/js/app.js
@@ -1,46 +1,36 @@
   $(document).ready(function () {
  	var filterVal = 'blur(5px)';
  	var filterN= 'blur(0)';
+  var filterTitle= 'blur(10px)';
+
+
 	$('.one').mouseover(function () {
  	$('.img_one').css('filter',filterVal)
-  	.css('webkitFilter',filterVal)
-  	.css('mozFilter',filterVal)
-  	.css('oFilter',filterVal)
-  	.css('msFilter',filterVal);
+  	.css('-webkitFilter',filterVal);
   	$('.one').mouseleave(function () {
  	$('.img_one').css('filter', filterN)
- 		.css('webkitFilter',filterN)
-  		.css('mozFilter',filterN)
-  		.css('oFilter',filterN)
-  		.css('msFilter',filterN);
+ 		.css('-webkitFilter',filterN);
  	});
  });
 	$('.two').mouseover(function () {
  	$('.img_two').css('filter',filterVal)
-  	.css('webkitFilter',filterVal)
-  	.css('mozFilter',filterVal)
-  	.css('oFilter',filterVal)
-  	.css('msFilter',filterVal);
+  	.css('-webkitFilter',filterVal);
   	$('.two').mouseleave(function () {
  	$('.img_two').css('filter', filterN)
- 		.css('webkitFilter',filterN)
-  		.css('mozFilter',filterN)
-  		.css('oFilter',filterN)
-  		.css('msFilter',filterN);
+ 		.css('-webkitFilter',filterN);
   	});
   });
 	$('.three').mouseover(function () {
  	$('.img_three').css('filter',filterVal)
-  	.css('webkitFilter',filterVal)
-  	.css('mozFilter',filterVal)
-  	.css('oFilter',filterVal)
-  	.css('msFilter',filterVal);
+  	.css('-webkitFilter',filterVal);
   	$('.three').mouseleave(function () {
  	$('.img_three').css('filter', filterN)
- 		.css('webkitFilter',filterN)
-  		.css('mozFilter',filterN)
-  		.css('oFilter',filterN)
-  		.css('msFilter',filterN);
+ 		.css('-webkitFilter',filterN);
   	});
   });
+  $('.bull, .author_name, .pull_one').addClass("filt");
+  $(window).scroll(function(){
+  $('.pull_one').removeClass("filt");
 });
+});
+
