5142dec8bc4f61e60353ae742036f92d087d01e2
Yasemin Varlik <varly065@newschool.edu>
2018-04-16 18:48:24 -0400

    article pages edited

--- publication/assets/js/issuethree.js
+++ publication/assets/js/issuethree.js
@@ -1,26 +1,39 @@
   $(document).ready(function () {
 
-  $('.urinal, .author_m_name').addClass("filt");
+  var distance = $('.pull_m_one').offset().top,
+    $window = $(window);
+    var distanceTwo = $('.pull_m_two').offset().top;
+    var distanceThree = $('.pull_m_four').offset().top;
+
+  $('.urinal, .author_m_name, .pull_m_one').addClass("filt");
   $(window).scroll(function(){
-  $('.pull_one, .pull_two, .pull_three').removeClass("filt");
+  $('.pull_m_one').removeClass("filt");
 });
 
-//   $window.scroll(function() {
-//     if ( $window.scrollTop() >= distance ) {
-//         $(".pull_one, .img_fc").css('opacity', 0);
-//     }
-//     if ( $window.scrollTop() < distance ) {
-//         $(".pull_one, .img_fc").css('opacity', 1);
-//     }
-// });
-//   $window.scroll(function() {
-//     if ( $window.scrollTop() >= distanceTwo) {
-//         $(".pull_four").css('opacity', 0);
-//     }
-//     if ( $window.scrollTop() < distanceTwo ) {
-//         $(".pull_four").css('opacity', 1);
-//     }
-// });
+  $window.scroll(function() {
+    if ( $window.scrollTop() >= distance ) {
+        $(".pull_m_one, .img_fount").css('opacity', 0);
+    }
+    if ( $window.scrollTop() < distance ) {
+        $(".pull_m_one, .img_fount").css('opacity', 1);
+    }
+});
+  $window.scroll(function() {
+    if ( $window.scrollTop() >= distanceTwo) {
+        $(".pull_m_two").css('opacity', 0);
+    }
+    if ( $window.scrollTop() < distanceTwo ) {
+        $(".pull_m_two").css('opacity', 1);
+    }
+});
+  $window.scroll(function() {
+    if ( $window.scrollTop() >= distanceThree) {
+        $(".pull_m_four").css('opacity', 0);
+    }
+    if ( $window.scrollTop() < distanceThree ) {
+        $(".pull_m_four").css('opacity', 1);
+    }
+});
 
   $(window).on("scroll", function() {
   var scrollHeight = $(document).height();
--- publication/assets/js/issuetwo.js
+++ publication/assets/js/issuetwo.js
@@ -1,8 +1,10 @@
   $(document).ready(function () {
 
-var distance = $('.pull_s_one').offset().top,
+  var distance = $('.pull_s_one').offset().top,
     $window = $(window);
-  // var distanceTwo = $('.pull_four').offset().top;
+  var distanceTwo = $('.pull_s_three').offset().top;
+  var distanceThree = $('.pull_s_four').offset().top;
+  var distanceFour = $('.pull_s_five').offset().top;
 
 
   $('.statue, .author_s_name, .pull_s_one').addClass("filt");
@@ -18,15 +20,31 @@ var distance = $('.pull_s_one').offset().top,
         $(".pull_s_one, .img_stat").css('opacity', 1);
     }
 });
-//   $window.scroll(function() {
-//     if ( $window.scrollTop() >= distanceTwo) {
-//         $(".pull_four").css('opacity', 0);
-//     }
-//     if ( $window.scrollTop() < distanceTwo ) {
-//         $(".pull_four").css('opacity', 1);
-//     }
-// });
+  $window.scroll(function() {
+    if ( $window.scrollTop() >= distanceTwo) {
+        $(".pull_s_three").css('opacity', 0);
+    }
+    if ( $window.scrollTop() < distanceTwo ) {
+        $(".pull_s_three").css('opacity', 1);
+    }
+});
 
+$window.scroll(function() {
+    if ( $window.scrollTop() >= distanceThree) {
+        $(".pull_s_four").css('opacity', 0);
+    }
+    if ( $window.scrollTop() < distanceThree) {
+        $(".pull_s_four").css('opacity', 1);
+    }
+});
+$window.scroll(function() {
+    if ( $window.scrollTop() >= distanceFour) {
+        $(".pull_s_five").css('opacity', 0);
+    }
+    if ( $window.scrollTop() < distanceFour) {
+        $(".pull_s_five").css('opacity', 1);
+    }
+});
   $(window).on("scroll", function() {
   var scrollHeight = $(document).height();
   var scrollPosition = $(window).height() + $(window).scrollTop();
